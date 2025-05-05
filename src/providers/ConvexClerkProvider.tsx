6244a90b51396f23bd022d7ed5a2ab57d3b7aa46
Djoleni <stevanojlo@gmail.com>
2025-05-05 22:24:31 +0200

    webhooks setup

--- src/providers/ConvexClerkProvider.tsx
+++ src/providers/ConvexClerkProvider.tsx
@@ -2,7 +2,7 @@
 
 import {ClerkProvider, useAuth} from '@clerk/nextjs';
 import {ConvexReactClient} from 'convex/react';
-import {ConvexProviderWithClerk} from 'convex/react-clerk;
+import {ConvexProviderWithClerk} from 'convex/react-clerk';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
